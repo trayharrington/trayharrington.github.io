04fa105782763e0c695834496232eb3c6cae6868
Tray Harrington <traylon.harrington@gmail.com>
2025-03-17 17:05:16 -0500

    Update gallery to change 'current' CSS class

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,6 @@
+// Activates the image gallery.
+// The main task is to attach an even listener to each image in the gallery
+// and respond appropriately on click.
 function activateGallery() {
 	let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
 	let mainImage  = document.querySelector("#gallery-photo img");
@@ -8,6 +11,11 @@ function activateGallery() {
 			let newImageSrc = thumbnail.dataset.largeVersion;
 			mainImage.setAttribute("src", newImageSrc);
 			mainImage.setAttribute("alt", thumbnail.alt);
+
+			// Change which image is current.
+			let currentClass = "current";
+			document.querySelector(".current").classList.remove(currentClass);
+			thumbnail.parentNode.classList.add(currentClass);
 		});
 	});
 }
