d3c5f443a43a34b68bf54113ea1381d8bf666407
Tray Harrington <traylon.harrington@gmail.com>
2025-03-17 16:54:50 -0500

    Update gallery to adjust main image via click

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,13 @@
 function activateGallery() {
-	alert("Hello from the gallery file!");
+	let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+	let mainImage  = document.querySelector("#gallery-photo img");
+
+	thumbnails.forEach(function(thumbnail) {
+		thumbnail.addEventListener("click", function() {
+			// Set clicked image as main image.
+			let newImageSrc = thumbnail.dataset.largeVersion;
+			mainImage.setAttribute("src", newImageSrc);
+			mainImage.setAttribute("alt", thumbnail.alt);
+		});
+	});
 }
