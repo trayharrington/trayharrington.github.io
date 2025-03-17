10728c799de580cdc1a526ef2298470fafa48b03
Tray Harrington <traylon.harrington@gmail.com>
2025-03-17 17:23:38 -0500

    Preload large images

--- js/gallery.js
+++ js/gallery.js
@@ -6,9 +6,13 @@ function activateGallery() {
 	let mainImage  = document.querySelector("#gallery-photo img");
 
 	thumbnails.forEach(function(thumbnail) {
+		// Preload large images.
+		let newImageSrc = thumbnail.dataset.largeVersion;
+		let largeVersion = new Image();
+		largeVersion.src = newImageSrc;
+
 		thumbnail.addEventListener("click", function() {
 			// Set clicked image as main image.
-			let newImageSrc = thumbnail.dataset.largeVersion;
 			mainImage.setAttribute("src", newImageSrc);
 			mainImage.setAttribute("alt", thumbnail.alt);
 
